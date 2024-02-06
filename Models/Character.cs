8d4230b627b701c93e78a0a6e52740aec220d677
marincicp <marincicpetar@gmail.com>
2024-02-06 22:49:41 +0100

    new commit

--- Models/Character.cs
+++ Models/Character.cs
@@ -7,14 +7,14 @@ namespace backendvjezba.Models
 {
     public class Character
     {
-        
-public int Id {get; set;}
-public string? Name {get; set;} = "Frodo";
-public int HitPoints {get;set;} = 100;
-public int Stength {get;set;} = 10;
-public int Defense  {get;set;} = 10;
-public int Intelligence {get; set;} = 10;
-public RpgClass Class {get; set;} = RpgClass.Knight;
+
+        public int Id { get; set; }
+        public string? Name { get; set; } = "Frodo";
+        public int HitPoints { get; set; } = 100;
+        public int Stength { get; set; } = 10;
+        public int Defense { get; set; } = 10;
+        public int Intelligence { get; set; } = 10;
+        public RpgClass Class { get; set; } = RpgClass.Knight;
 
     }
 }
--- Models/ServiceResponse.cs
+++ Models/ServiceResponse.cs
@@ -5,7 +5,6 @@ using System.Threading.Tasks;
 
 namespace backendvjezba.Models
 {
-    // T is actuial type of data we want to return
     public class ServiceResponse<T>
     {
         public T? Data { get; set; }
